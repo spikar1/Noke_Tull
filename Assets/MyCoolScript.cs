fb9bf6180450751844abe9987dd11ac5e80143e4
Steffen M. Fureli <Steffen.fureli@gmail.com>
2022-03-11 10:55:33 +0100

    Update MyCoolScript.cs

--- Assets/MyCoolScript.cs
+++ Assets/MyCoolScript.cs
@@ -7,12 +7,12 @@ public class MyCoolScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Change some behaviour on player controller
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int i = 10;
     }
 }
